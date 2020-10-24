afcf81a1edfca5e64074f7f751847effc0272217
Dmitry Mozzherin <dmozzherin@gmail.com>
2020-10-24 05:35:51 -0500

    typo

--- data/data_json/save.go
+++ data/data_json/save.go
@@ -29,11 +29,7 @@ func (e EngineJSON) Save(cs *entity.CardStack) error {
 }
 
 func addNewWords(cs *entity.CardStack, num int) {
-<<<<<<< HEAD
 	if num <= 15 {
-=======
-	if num <= 20 {
->>>>>>> da7b8ca (Close #6 remember replies)
 		return
 	}
 
